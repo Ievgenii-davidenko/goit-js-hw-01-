84519316457e6460f7fb1cdcb4ec91bf6a0638d3
Ievgenii <bijibnui88@gmail.com>
2020-03-09 18:03:21 +0200

    added index.html

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+'use strict';
+const total = 100;
+let ordered = 50;
+
+if (ordered > total) {
+  console.log('На складе недостаточно товаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
+
+ordered = 50;
+if (ordered > total) {
+  console.log('На складе недостаточно товаров!');
+} else {
+  console.log ( 'Заказ оформлен, с вами свяжется менеджер');
+}
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+'use strict';
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+
+const logInn = prompt('Введите пароль');
+
+if (logInn === null) {
+  message = `Отменено пользователем!`;
+} else if (logInn === ADMIN_PASSWORD) {
+  message = `Добро пожаловать!`;
+} else {
+  message = `Доступ запрещен, неверный пароль!`;
+}
+
+console.log(message);
